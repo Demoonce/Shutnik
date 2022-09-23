f5e74491e8db7e7860a0348cc137da769e29b5e5
Demoonce <pavel.jujkov@gmail.com>
2022-09-23 17:42:20 +0400

    Added jokes parser

--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ go 1.19
 
 require github.com/go-vk-api/vk v0.0.0-20200129183856-014d9b8adc96
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"crypto/rand"
 	"encoding/gob"
+	"fmt"
 	"log"
 	"math/big"
+	"net/http"
 	"os"
 	"time"
-	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 	"github.com/go-vk-api/vk"
 )
 
 var (
-	KEY    string
-	Jokes  []string
-	Client *vk.Client
+	KEY string
+
+	Jokes     []string
+	Client    *vk.Client
+	JokesChan chan string
 )
 
 func getRandomJoke(jokes []string) string {
@@ -25,18 +30,55 @@ func getRandomJoke(jokes []string) string {
 	return jokes[num.Int64()]
 }
 
+func getJokesPage(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	jokes, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	jokes.Find("div.text").Each(func(i int, s *goquery.Selection) {
+		Jokes = append(Jokes, s.Text())
+	})
+}
+
+func ParseJokes() {
+	for a := 1; a < 500; a++ {
+		time.Sleep(time.Millisecond * 10)
+		go getJokesPage(fmt.Sprintf("https://nekdo.ru/page/%d", a))
+	}
+loop:
+	for {
+		select {
+		case <-time.NewTimer(time.Second * 5).C:
+			close(JokesChan)
+			break loop
+		case a := <-JokesChan:
+			Jokes = append(Jokes, a)
+		}
+	}
+}
+
+func WriteJokes() {
+	file, err := os.Create("jokes.gob")
+	defer file.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	encoder := gob.NewEncoder(file)
+	encoder.Encode(Jokes)
+}
+
 func init() {
 	KEY = os.Getenv("KEY")
 	if KEY == "" {
 		log.Fatalln("Specify a key for application")
 	}
-	file, err := os.Open("jokes.gob")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-	decoder := gob.NewDecoder(file)
-	decoder.Decode(&Jokes)
+	var err error
 	Client, err = vk.NewClientWithOptions(vk.WithToken(KEY))
 	if err != nil {
 		log.Fatalln(err)
@@ -54,8 +96,22 @@ func sendJoke() {
 	}
 }
 
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Shutnik started"))
+}
+
 func main() {
-	go http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", Index)
+	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	ParseJokes()
+	WriteJokes()
+	file, err := os.Open("jokes.gob")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	decoder.Decode(&Jokes)
 	post_ticker := time.NewTicker(time.Hour * 3)
 	sendJoke()
 	for {
